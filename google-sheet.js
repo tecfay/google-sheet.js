82f9f592538a81a9c4ae859d7cb50f945d574742
tecfay <45577071+tecfay@users.noreply.github.com>
2020-02-19 00:01:06 +0000

    Update google-sheet.js

--- google-sheet.js
+++ google-sheet.js
@@ -59,7 +59,7 @@ $("#request-form").submit(function(event){
         // Reenable the inputs
         $inputs.prop("disabled", false);
         console.log("It's running");
-        window.location.href = 'https://dreamaco.youcan.shop';
+        window.location.href = 'dreamaco.youcan.shop';
     });
 
     // Prevent default posting of form
